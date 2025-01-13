d46d30fa202d23ca208d5b9176b333e01286af74
MazenMoh2011 <mazenalnagar203@gmail.com>
2025-01-13 15:37:47 +0300

    gbhujkvgyit

---
+++ A3.py
@@ -0,0 +1,6 @@
+from datetime import time
+time= time(11, 34, 56, 6968)
+print("hour=", time.hour)
+print("minute=", time.minute)
+print("second=", time.second)
+print("microsecond", time.microsecond)
