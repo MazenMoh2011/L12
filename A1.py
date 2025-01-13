1f749ff0974f6b89f90dc61b7bf764df66fdffdb
MazenMoh2011 <mazenalnagar203@gmail.com>
2025-01-13 15:27:07 +0300

    gererg

--- A1.py
+++ A1.py
@@ -1,3 +1,8 @@
-import datetime
-x= datetime.datetime.now()
+from datetime import date, time, datetime
+x= datetime.now()
 print(x)
+today = date.today()
+now= datetime.now()
+print("today's date is", today)
+print("\ncurrent date and time is", now)
+print("\ndate components", today.year, today.month, today.day)
